b77a58254e5011915a1d87b7fa9aff71e23bd0ff
Anny <101923279+AnnyVJu@users.noreply.github.com>
2023-06-27 01:35:49 +0300

    Add model Day

---
+++ lib/domain/model/day.dart
@@ -0,0 +1,15 @@
+import 'package:meta/meta.dart';
+
+class Day {
+  final int dayLength;
+  final DateTime sunset;
+  final DateTime sunrise;
+  final DateTime solarNoon;
+
+  Day({
+    required this.dayLength,
+    required this.sunset,
+    required this.sunrise,
+    required this.solarNoon,
+});
+}
