e0afdbbc576c22323fae7c67ffc92f556fc09422
Anny <101923279+AnnyVJu@users.noreply.github.com>
2023-11-10 00:43:45 +0300

    Update UI

--- lib/presentation/home.dart
+++ lib/presentation/home.dart
@@ -1,7 +1,8 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:sunset/domain/state/home/home_state.dart';
-import '../domain/model/day.dart';
 import '../internal/dependencies/home_module.dart';
 
 class Home extends StatefulWidget {
@@ -25,45 +26,100 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: FocusScope.of(context).unfocus,
-      child: Scaffold(
-        body: _getBody(),
+    Size size = MediaQuery.of(context).size;
+   // double containerHeight = size.height * 0.90;
+    return MaterialApp(
+      theme: ThemeData(colorScheme: const ColorScheme.highContrastDark()),
+      home: GestureDetector(
+        onTap: FocusScope.of(context).unfocus,
+        child: Scaffold(
+          body: _getBody(),
+        ),
       ),
     );
   }
 
   Widget _getBody() {
     return SafeArea(
-      child: Padding(
-        padding: const EdgeInsets.all(10),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            _getRowInput(),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: _getDay,
-              child: const Text('Получить'),
+      child: Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: Image.asset("assets/images/2.png").image,
+                  fit: BoxFit.cover)),
+          child: Center(
+            child: ClipRRect(
+              borderRadius: borderRadius,
+              child: BackdropFilter(
+                filter: ImageFilter.blur(sigmaX: 5.0, sigmaY: 5.0),
+                child: Container(
+                  width: 350,
+                  height: 350,
+                  decoration: BoxDecoration(
+                      boxShadow: [
+                        BoxShadow(
+                            color: Colors.red.withOpacity(0.2), blurRadius: 10)
+                      ],
+                      borderRadius: borderRadius,
+                      border: Border.all(
+                          color: Colors.white.withOpacity(0.5), width: 1.0),
+                      gradient: LinearGradient(
+                          begin: Alignment.topLeft,
+                          end: Alignment.bottomRight,
+                          colors: [
+                            Colors.white.withOpacity(0.2),
+                            Colors.white.withOpacity(0.1)
+                          ],
+                          stops: const [
+                            0.0,
+                            1.0
+                          ])),
+                  child: Center(
+                    child: Padding(
+                      padding: const EdgeInsets.all(20),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          _getRowInput(),
+                          const SizedBox(height: 20),
+                          OutlinedButton(
+                            style: const ButtonStyle(
+                              padding:
+                                  MaterialStatePropertyAll(EdgeInsets.all(10)),
+                            ),
+                            onPressed: _getDay,
+                            child: const Text('Получить'),
+                          ),
+                          const SizedBox(height: 20),
+                          _getDayInfo(),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
             ),
-            const SizedBox(height: 20),
-            _getDayInfo(),
-          ],
+          ),
         ),
       ),
     );
   }
 
   Widget _getRowInput() {
-    return Row(
+    return Column(
       children: [
-        Expanded(
-          child: TextField(
-            controller: _latController,
-            decoration: const InputDecoration(hintText: 'Широта'),
-            keyboardType: const TextInputType.numberWithOptions(
-                decimal: true, signed: true),
-          ),
+        TextField(
+          controller: _latController,
+          decoration: const InputDecoration(hintText: 'Широта'),
+          keyboardType: const TextInputType.numberWithOptions(
+              decimal: true, signed: true),
+        ),
+        const SizedBox(height: 20),
+        TextField(
+          controller: _lngController,
+          keyboardType: const TextInputType.numberWithOptions(
+              decimal: true, signed: true),
+          decoration: const InputDecoration(hintText: 'Долгота'),
         ),
       ],
     );
@@ -84,7 +140,8 @@ class _HomeState extends State<Home> {
             Text('Восход: ${_homeState.day?.sunrise?.toLocal()}'),
             Text('Заход: ${_homeState.day?.sunset?.toLocal()}'),
             Text('Полдень: ${_homeState.day?.solarNoon?.toLocal()}'),
-            Text('Продолжительность дня: ${Duration(seconds: _homeState.day!.dayLength)}'),
+            Text(
+                'Продолжительность дня: ${Duration(seconds: _homeState.day!.dayLength)}'),
           ],
         );
       },
@@ -97,3 +154,5 @@ class _HomeState extends State<Home> {
     _homeState.getDay(latitude: lat, longitude: lng);
   }
 }
+
+final borderRadius = BorderRadius.circular(20.0);
